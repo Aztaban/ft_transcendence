9807f28c40329af737d6d72a0b7157f440a81f01
Aztaban <Aztabanee@gmail.com>
2026-09-26 02:03:55 +0200

    fix(security): trust HTTPS scheme from nginx proxy

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -8,6 +8,8 @@ DEBUG = False
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 ALLOWED_HOSTS = [
     host.strip() for host in os.environ["DJANGO_ALLOWED_HOSTS"].split(",") if host.strip()
 ]
